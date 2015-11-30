0c6a03fdb9206dfede7a70698e5ae707f8ad10d2
speed <k78@qq.com>
2015-11-30 23:36:06 +0800

    u

---
+++ start.sh
@@ -0,0 +1,10 @@
+shDir=$(cd "$(dirname "$0")"; pwd)
+
+cd ${shDir}/jetty/
+ 
+chmod +rwx ${shDir}/jre -R
+
+export PATH=$PATH:${shDir}/jre/bin/
+
+java -version
+java -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500
