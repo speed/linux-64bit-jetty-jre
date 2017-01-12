b32bd52e1baf02c67bd1e554c5223a69ac121ac5
speed <k78@qq.com>
2017-01-12 22:28:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,13 @@ export PATH=${shDir}/jre/bin/
 
 java -version
 
+echo 'test'
+
+echo ${shDir}
+
+echo 'test2'
+
 #(java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 > /dev/null &)
 
 
-(java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 &)
+java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 &
