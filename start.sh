1e8d945d57ab81fed6bc0cb884e303c63fa131f3
speed <k78@qq.com>
2017-01-12 21:52:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 shDir=$(cd "$(dirname "$0")"; pwd)
 
 cd ${shDir}/jetty/
@@ -5,4 +7,8 @@ cd ${shDir}/jetty/
 export PATH=${shDir}/jre/bin/
 
 java -version
-(java $JAVA_OPTS -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 > /dev/null &)
+
+#(java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 > /dev/null &)
+
+
+(java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 &)
