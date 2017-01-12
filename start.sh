08f93efe70a304d3ca62c0c94f50fa33b3520cd3
speed <k78@qq.com>
2017-01-13 00:09:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,6 @@ export PATH=${shDir}/jre/bin/
 
 java -version
 
-sed -ie 's/jdbc:hsqldb:file:~\/newcrawler\/db\/spider/jdbc:hsqldb:file:\/opt\/newcrawler\/db\/spider/g' /opt/newcrawler/war/WEB-INF/classes/datanucleus.properties
-
 #(java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 > /dev/null &)
 
 java ${JAVA_OPTS} -jar /opt/newcrawler/jetty/start.jar jetty.port=8500 &
