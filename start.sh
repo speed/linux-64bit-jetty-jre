306090c53a7315b4847796323e484823e7ea83a3
speed <k78@qq.com>
2017-01-13 12:58:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,5 +12,5 @@ echo ${JAVA_OPTS}
 
 #(java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar start.jar jetty.port=8500 > /dev/null &)
 
-java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar /opt/newcrawler/jetty/start.jar jetty.port=8500
+java ${JAVA_OPTS} -DSTOP.PORT=8504 -DSTOP.KEY=stop_jetty -jar /opt/newcrawler/jetty/start.jar jetty.port=8500 &
 
